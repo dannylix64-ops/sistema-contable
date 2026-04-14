2475f8b27bce684401b66da6319d08f82413c337
dannylix64-ops <dannylix64@gmail.com>
2026-04-14 14:45:39 -0500

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,4 +5,14 @@ db = sqlite3.connect("contabilidad.db")
 db.execute("CREATE TABLE usuarios (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
 db.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY, nombre TEXT)")
 
+db.execute("""
+CREATE TABLE transacciones (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tipo TEXT,
+    descripcion TEXT,
+    monto REAL,
+    fecha TEXT
+)
+""")
+
 db.commit()
